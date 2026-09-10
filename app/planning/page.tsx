76c78c2099839dd655748a765b2c9d5e1ff97252
Natchlou <n.jullien57@gmail.com>
2026-09-11 00:03:56 +0200

    fix(planning): remove debug log and clean up layout structure

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -10,28 +10,21 @@ export default async function Planning() {
     .from('planning')
     .select('*')
     .order('date', { ascending: false });
-<<<<<<< HEAD
-  console.log(planning, error)
-  if (error) {
-=======
 
-    if (error) {
->>>>>>> 134acba (feat(planning): implement monthly planning generation and saving functionality)
+  if (error) {
     console.error("Erreur lors de la récupération des plannings :", error);
   }
   return (
     <div className="flex flex-col flex-1 min-w-7xl mx-auto px-4 py-8 font-sans dark:bg-black">
-<<<<<<< HEAD
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold mb-2">Votre planning en 1 click</h1>
-=======
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold">Votre planning en quelques clicks</h1>
->>>>>>> 134acba (feat(planning): implement monthly planning generation and saving functionality)
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-4xl font-bold">Votre planning en quelques clicks</h1>
+        </div>
+        <main>
+          <PlanningMonthly data={planning ? planning[0] : []} />
+        </main>
       </div>
-      <main>
-        <PlanningMonthly data={planning ? planning[0] : []}/>
-      </main>
     </div>
   );
 }
